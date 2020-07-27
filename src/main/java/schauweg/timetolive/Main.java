f9e702510ac04b91870f64bcf950ab97c09263f2
Felix Heß <felix.hess@freenet.de>
2020-07-27 20:23:21 +0200

    Update to Minecraft 1.16.1 and added Creeper support

--- src/main/java/schauweg/timetolive/Main.java
+++ src/main/java/schauweg/timetolive/Main.java
@@ -1,22 +1,11 @@
 package schauweg.timetolive;
 
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.common.Mod;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import net.fabricmc.api.ClientModInitializer;
 
-// The value here should match an entry in the META-INF/mods.toml file
-@Mod(Main.MOD_ID)
-public class Main
-{
+public class Main implements ClientModInitializer {
 
-    public static final String MOD_ID = "timetolive";
+    @Override
+    public void onInitializeClient() {
 
-    // Directly reference a log4j logger.
-    private static final Logger LOGGER = LogManager.getLogger();
-
-    public Main() {
-        MinecraftForge.EVENT_BUS.register(new TNTCountdownRenderer());
     }
-
 }
