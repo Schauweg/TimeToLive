cd5695efdbf0a8548b0b03ce90e33a189e318c9d
Felix Heß <felix.hess@freenet.de>
2020-07-27 20:25:45 +0200

    Update to Minecraft 1.16.1 and added Creeper support

---
+++ src/main/java/schauweg/timetolive/mixin/WorldRendererMixin.java
@@ -0,0 +1,24 @@
+package schauweg.timetolive.mixin;
+
+import net.minecraft.client.render.*;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.util.math.Matrix4f;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import schauweg.timetolive.CountdownRenderer;
+
+@Mixin(WorldRenderer.class)
+public class WorldRendererMixin {
+
+    @Shadow
+    private Frustum capturedFrustum;
+
+    @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/WorldRenderer;checkEmpty(Lnet/minecraft/client/util/math/MatrixStack;)V", ordinal = 0))
+    private void render(MatrixStack matrices, float tickDelta, long limitTime, boolean renderBlockOutline, Camera camera, GameRenderer gameRenderer, LightmapTextureManager lightmapTextureManager, Matrix4f projection, CallbackInfo ci) {
+        CountdownRenderer.render(matrices, tickDelta, camera, gameRenderer, lightmapTextureManager, projection, this.capturedFrustum);
+    }
+
+}
