f6a73bd928de93ae741fe536b08c88294436eaf8
Felix Heß <felix.hess@freenet.de>
2021-06-09 02:18:29 +0200

    updating mod menu integration and gradle version

--- src/main/java/schauweg/timetolive/modmenu/TTLModMenuIntegration.java
+++ src/main/java/schauweg/timetolive/modmenu/TTLModMenuIntegration.java
@@ -1,7 +1,7 @@
 package schauweg.timetolive.modmenu;
 
-import io.github.prospector.modmenu.api.ConfigScreenFactory;
-import io.github.prospector.modmenu.api.ModMenuApi;
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
 import schauweg.timetolive.config.TTLConfigScreen;
 
 
